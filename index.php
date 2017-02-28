6464113cf519592338fbac429d893d48e554bbd0
Vit <hutki@tut.by>
2017-02-28 17:02:20 +0300

    1 день

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head lang="ru">
+    <meta charset="UTF-8">
+    <title>Главная</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="Регистрал">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <table class="main_border" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>&nbsp</td>
+        </tr>
+         <tr>
+            <td>
+               
+            </td>
+        </tr>
+         <tr>
+            <td>&nbsp</td>
+        </tr>
+    </table>
+    
+</body>
+</html>
